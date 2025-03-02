1050774beca3822adf7e1f4f2e120ab00d065e8b
Niels Bergsma <n.bergsma@gmail.com>
2025-03-02 20:23:09 +0100

    disable css hover state on touch centric devices

--- frontend/backoffice/tailwind.config.js
+++ frontend/backoffice/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{elm,html,js}", "./src/Main.elm", "./index.html"],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     extend: {},
   },
